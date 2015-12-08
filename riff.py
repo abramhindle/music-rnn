510d9b4a7c996848bcc4353e6d87609886164e47
Abram Hindle <abram.hindle@softwareprocess.es>
2015-12-07 22:03:21 -0700

    no clue if this stuff works

--- riff.py
+++ riff.py
@@ -10,6 +10,8 @@ import quantizer
 # code clone
 parser = argparse.ArgumentParser(description='Estimate MIDI')
 parser.add_argument('scores', help='scores',nargs='+')
+parser.add_argument('-brain',default="regressor.pkl",help="Brain to use")
+parser.add_argument('-idiot',default=False,help="Idiot correction")
 # parser.add_argument('lin', help='Path of (.midi) label files for training/validation/testing')
 #parser.add_argument('tmp', help='Path of directory to write intermediary or data save files',default="./tmp/")
 args = parser.parse_args()
@@ -18,6 +20,24 @@ scores = args.scores
 # code clone
 qscores = [quantizer.convert_file(filename) for filename in scores]
 
-network = pickle.load(file("regressor.pkl"))
-our_score = np.array(qscores[0][0:1024],dtype=np.float32)
-network.predict(np.array([our_score]))
+network = pickle.load(file(args.brain))
+# stupid hack for a bad brain
+our_score = np.array(qscores[0][15000:],dtype=np.float32)
+if (args.idiot):
+    our_score = our_score[:,0:576]
+    
+mtest = np.array([our_score])
+preds = network.predict( mtest )
+descs = [quantizer.dl_2_desc(pred) for pred in preds[0]]
+# get IDs for anything that's defined
+ids = [i for i in range(0,len(our_score)) if ((our_score[i]!=0.0).any())]
+defdescs = [descs[i] for i in ids]
+
+input = np.array([our_score[ids]])
+# y = network.predict_sequence(mtest[0],100)
+for i in range(0,100):
+    y = network.predict(input)
+    z = quantizer.dl_2_desc(  y[0][-1] )
+    print z
+    input = np.array([np.concatenate((input[0],y[0][-1:]))])
+
